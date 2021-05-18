1ab598d66e02491983b6bf5aa791b51f1eaaf375
root <root@DESKTOP-PE1SVJ5.localdomain>
2021-05-18 10:48:06 +0800

    update run.sh of ncpa

--- 2-steps/ncpa/run.sh
+++ 2-steps/ncpa/run.sh
@@ -1,1 +1,1 @@
-podman run --privileged --name nagiosxi-agent -v ncpa_etc:/usr/local/ncpa/etc -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 5693:5693 -d nagiosxi-agent:2.3.1-1
+podman run --privileged --name nagiosxi-agent -v ncpa_etc:/usr/local/ncpa/etc -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 0.0.0.0:5693:5693 -d nagiosxi-agent:2.3.1-1
